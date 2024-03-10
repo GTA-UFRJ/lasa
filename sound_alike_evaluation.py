e396a6b903130f74b7172f2bc908b8920b2c7692
Rodrigo de Souza Couto <rodrigo@gta.ufrj.br>
2024-03-10 19:26:57 -0300

    adding clustering

--- sound_alike_evaluation.py
+++ sound_alike_evaluation.py
@@ -12,6 +12,12 @@ from collections import Counter
 import concurrent.futures
 from concurrent.futures import ProcessPoolExecutor
 from metaphoneptbr import phonetic
+from collections import Counter
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import MDS
 
 
 
@@ -107,6 +113,8 @@ class EvaluateSimilarity:
         self.product_substance_map = product_substance_map
         self.product_substance_original_map = product_substance_original_map
         self.similarities = {}
+        self.distances_count = None
+        self.sorted_distances = None
 
     def print_similarity(self,pair,similarity,outputFile = None):
         word1 = pair.split("-")[0]
@@ -118,7 +126,7 @@ class EvaluateSimilarity:
         print("PRODUTO B - Nome: " + str(word2) + " - Substancia: " + str(self.product_substance_original_map.get(word2,None)),file=outputFile)
         print("Distancia = " + str(similarity),file=outputFile)
 
-    def print_top_k_similarity(self,outputFileName='',k=1000):
+    def print_top_k_similarity(self,k=1000,outputFileName=''):
 
         sorted_similarities = sorted(self.similarities.items(), key=lambda item: item[1], reverse=False)
 
@@ -131,6 +139,75 @@ class EvaluateSimilarity:
         for pair, similarity in sorted_similarities_n_more_similar:
             self.print_similarity(pair,similarity,outputFile)
         outputFile.close()
+
+    def print_pairs_with_distance_d(self, d, outputFileName=''):
+
+        sorted_similarities = sorted(self.similarities.items(), key=lambda item: item[1], reverse=False)
+
+        # Filterng similarities to keep those equal or less than d
+        filtered_similarities = [(pair, sim) for pair, sim in sorted_similarities if sim <= float(float(d)+0.001)]
+
+        if outputFileName == '':
+            outputFile = None
+        else:
+            outputFile = open(outputFileName, 'w')
+
+        for pair, similarity in filtered_similarities:
+            self.print_similarity(pair, similarity, outputFile)
+
+        # Fechando o arquivo, se necessário
+        if outputFile is not None:
+            outputFile.close()
+
+    # Lembre-se de substituir 'self.similarities' pelo seu dicionário contendo as distâncias
+    # E também de adaptar 'self.print_similarity()' se necessário, conforme a sua implementação atual
+
+
+
+    def countSimilarities(self):
+        #Building a list with all similarities
+        distances = list(self.similarities.values())
+
+        #Counting the number of pairs with a given similarity
+        self.distances_count = Counter(distances)
+
+        # Sorting the distances
+        self.sorted_distances = sorted(self.distances_count.keys())
+
+        print("A lista abaixo mostra a quantidade de pares para uma dada distancia:")
+        for distance in self.sorted_distances:
+            print("Distancia: " + str(distance) + " Quantidade: " + str(self.distances_count[distance]))
+
+
+    #Build an Histogram for up to distance N
+    def build_distance_graph(self,fileNamePrefix,N=None):
+
+        if ((self.distances_count == None) or (self.sorted_distances == None)):
+            self.countSimilarities()
+
+        if (N == None):
+            N = self.sorted_distances[len(self.sorted_distances)-1]
+
+        #The counts for a given distance
+        counts = [self.distances_count[distance] for distance in self.sorted_distances]
+
+        # Considering only count equal or less than N
+        filtered_distances = [distance for distance in self.sorted_distances if distance <= N]
+        filtered_count = [counts[i] for i, distance in enumerate(self.sorted_distances) if distance <= N]
+
+        # Generating the graph
+        plt.figure(figsize=(10, 6))
+        plt.bar(filtered_distances, filtered_count, color='skyblue', edgecolor='black')
+        plt.title('Distance Between Pairs Histogram')
+        plt.xlabel('Distance')
+        plt.ylabel('Number of pairs')
+        plt.xticks(filtered_distances)
+        plt.grid(axis='y', alpha=0.75)
+
+        # Saving the graph in pdf
+        plt.savefig(fileNamePrefix+'_distanceGraph_'+str(N).replace(".", "_")+'.pdf', format='pdf')
+
+
 
     def same_substance(self, product1, product2):
         substance1 = self.product_substance_map.get(product1, None)
@@ -163,6 +240,52 @@ class EvaluateSimilarity:
                         self.similarities[key] = self.similarity_levenshtein(word1, word2)
     """
 
+    #TODO this function shoudl only run when self.similarities is available
+    def calculate_distance_matrix_from_dict(self):
+        size = len(self.words)
+        distance_matrix = np.zeros((size, size))
+        for i in range(size):
+            for j in range(i + 1, size):
+                key = f"{self.words[i]}-{self.words[j]}"
+                if key in self.similarities:
+                    dist = self.similarities[key]
+                else:
+                    # Try the inverse key
+                    key = f"{self.words[j]}-{self.words[i]}"
+                    if key in self.similarities:
+                        dist = self.similarities.get(key, 0)  # Assume 0 se não encontrar; ajuste conforme necessário
+                    else:
+                        #This case happens when the medications have the same substance
+                        #Hence, we put an infinite distance (50) between them
+                        #TODO 50 is hardcoded due to previous experimentos (there is no distance larger than 49.5)
+                        dist = 50
+                distance_matrix[i, j] = dist
+                distance_matrix[j, i] = dist
+        return distance_matrix
+
+    def cluster_and_plot_dbscan(self, fileNamePrefix,eps=2, min_samples=2):
+        distance_matrix = self.calculate_distance_matrix_from_dict()
+        db = DBSCAN(eps=eps, min_samples=min_samples, metric="precomputed")
+        labels = db.fit_predict(distance_matrix)
+
+        # Aplica MDS para visualização
+        mds = MDS(n_components=2, dissimilarity="precomputed", random_state=42)
+        positions = mds.fit_transform(distance_matrix)
+
+        plt.figure(figsize=(10, 8))
+        plt.scatter(positions[:, 0], positions[:, 1], c=labels, cmap='viridis', s=100)
+
+        for i, word in enumerate(self.words):
+            plt.text(positions[i, 0], positions[i, 1] + 0.02, word, ha='center')
+
+        plt.title('Palavras Clusterizadas com DBSCAN e Distância de Levenshtein')
+        plt.xlabel('MDS 1')
+        plt.ylabel('MDS 2')
+
+        #Saving graph
+        plt.savefig(fileNamePrefix+'_cluster.pdf', format='pdf')
+
+
     def calculate_similarities_for_batch(self, batch):
         batch_results = {}
         for pair in batch:
@@ -182,7 +305,7 @@ class EvaluateSimilarity:
             yield pairs[i:i + num_batches]
 
     #num_processes paralelize the evaluation, dividind the pair into batches
-    def run(self, num_processes=None):
+    def run(self, num_processes=1):
         pairs = [(self.words[i], self.words[j]) for i in range(len(self.words)) for j in range(i + 1, len(self.words))]
         num_batches = len(pairs) // num_processes if num_processes else len(pairs)
 
@@ -205,19 +328,51 @@ def main(mode):
         print("  todos: Comparando todos (comerciais e DCB)")
         sys.exit(1)
 
+    #Defining the path to store results
+    path = './output'
+    if not os.path.exists(path):
+        # O diretório não existe; criando-o
+        os.makedirs(path)
+
+    fileNamePrefix = path + str("/") + mode
+
     #Generating the word list based on the chosen mode
     print("Lendo o arquivo para gerar a lista de palavras")
-    word_list_generator = GenerateWordList(mode,'./listaMedicamentos.xls')
+    word_list_generator = GenerateWordList(mode,'./listaMedicamentosTeste.xls')
     word_list_generator.run()
 
     print("Calculando as similaridades")
     #Evaluating similarities
     similarity_evaluator = EvaluateSimilarity(word_list_generator.words,word_list_generator.product_substance_map, word_list_generator.product_substance_original_map)
-    similarity_evaluator.run(8)
+    #The run method will receive the number of processes to create
+    #In this case we will use all the available cores
+    similarity_evaluator.run(int(os.cpu_count()))
 
+    print("Salvando arquivo com as k similaridades")
     #Print top K similairties
-    #TODO modify hardcoded name
-    similarity_evaluator.print_top_k_similarity("top1000fonemas.txt")
+    #TODO modify hardcoded k
+    k = 100
+    similarity_evaluator.print_top_k_similarity(k,fileNamePrefix+"_top_" + str(k) + ".txt")
+
+    print("Salvando arquivo com similaridade ate 1")
+    #Print top K similairties
+    #TODO modify hardcoded k
+    d = 1
+    similarity_evaluator.print_pairs_with_distance_d(d,fileNamePrefix+"_similarityUpTo_" + str(d) + ".txt")
+
+    print("Salvando arquivo com similaridade ate 2")
+    #Print top K similairties
+    #TODO modify hardcoded k
+    d = 2
+    similarity_evaluator.print_pairs_with_distance_d(d,fileNamePrefix+"_similarityUpTo_" + str(d) + ".txt")
+
+    print("Salvando arquivo com histograma")
+    similarity_evaluator.build_distance_graph(fileNamePrefix)
+
+    print("Salvando arquivo com histograma até distancia 2")
+    similarity_evaluator.build_distance_graph(fileNamePrefix,2)
+
+    similarity_evaluator.cluster_and_plot_dbscan(fileNamePrefix)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Programa opera em um dos tres modos.')
